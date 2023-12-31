78e749788846cec7704c3d6d947009c0376a7361
BetMul <1148347151@qq.com>
2023-12-31 16:10:22 +0800

    my first version of 2023 record

--- script/articleImage.js
+++ script/articleImage.js
@@ -8,11 +8,9 @@ class articleImage extends HTMLElement {
 
         this.innerHTML =`
             <br>
-            <br>
             <div style="display: flex;">
-            <img src="${imageSrc}" width="90%" height="90%" style="margin: auto;">
+            <img src="${imageSrc}" width="90%" style="margin: auto;">
             </div>
-            <br>
             <br>
         `;
     }
